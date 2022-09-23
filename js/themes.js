8a13b0f09cb4e23b28549dfc5c06995ae264733a
jakegodsall <jake.edward.godsall@gmail.com>
2022-09-23 16:25:02 +0100

    added animations for  radio buttons popping into existence

--- js/themes.js
+++ js/themes.js
@@ -1,12 +1,21 @@
-const radioButtons = document.getElementsByName('theme');
+const radioButtons = document.querySelectorAll('#radio-button');
 
 const changeTheme = (e) => {
-    const theme = e.target.value;
-    console.log(e.target.value);
+    radioButtons.forEach((btn) => {
+        if (btn.classList.contains('active')) {
+            btn.classList.remove('active');
+        }
+    });
+    e.target.classList.toggle('active');
+    const theme = e.target.classList[1];
 
-    document.body.className = theme;
+    console.log(theme);
+
+    // document.body.className = theme;
 };
 
 radioButtons.forEach((button) => {
     button.addEventListener('click', changeTheme);
 });
+
+console.log(radioButtons);
