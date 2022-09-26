b219bab91976e2976540210f38d6ab31463b0143
jakegodsall <jake.edward.godsall@gmail.com>
2022-09-26 10:24:14 +0100

    created ES6 class for the calculator and built all calculation logic, as well as adding button event listeners.

--- js/calculator-logic.js
+++ js/calculator-logic.js
@@ -1,152 +1,167 @@
 // ----------------- DOM ELEMENTS -----------------
 const screenText = document.getElementById('screen-text');
-const buttons = document.getElementsByTagName('button');
+const buttons = document.getElementsByClassName('button');
+
+const decimalButton = document.getElementById('button-decimal');
+const delButton = document.getElementById('button-del');
 const equalsButton = document.getElementById('button-equals');
+const resetButton = document.getElementById('button-reset');
+const numericalButtons = [];
+const operationButtons = [];
 
-// ----------------- UTILITY FNS -----------------
-
-// print number to screen
-const printNumber = (val) => {
-    if (screenText.innerText === '0' && val !== '.') {
-        screenText.innerText = val;
-    } else {
-        screenText.innerText += val;
-    }
-};
-
-// delete last character from screen
-const delOneChar = () => {
-    if (screenText.innerText.length === 1) {
-        screenText.innerText = '0';
-    } else {
-        screenText.innerText = screenText.innerText.slice(0, screenText.innerText.length - 1);
-    }
-};
-
-const addDecimal = () => {
-    if (!screenText.innerText.includes('.')) {
-        printNumber('.');
-    }
-};
-
-// clear all input
-const clearInput = () => {
-    screenText.innerText = '0';
-};
-
-// addition
-const addNumbers = () => {
-    const firstValue = screenText.innerText;
-    screenText.innerText = '0';
-
-    equalsButton.addEventListener('click', () => {
-        const secondValue = screenText.innerText;
-
-        const result = +firstValue + +secondValue;
-
-        clearInput();
-        printNumber(result);
-    });
-};
-
-// click equals
-const calculateValue = () => {
-    values.push(screenText.innerText);
-    console.log(values);
-
-    const strValues = values.join(' ');
-    console.log(strValues);
-    const result = eval(strValues);
-    console.log(result);
-
-    clearInput();
-    printNumber(result);
-
-    // reset values
-    values = [];
-};
-
-// add value to array
-const pushToOperationsArray = (e) => {
-    const button = e.target;
-    if (screenText.innerText !== '0') {
-        values.push(screenText.innerText);
-    }
-    console.log(values);
-
-    // allowing for changing operation
-    if (!'+-*/'.includes(values[values.length - 1])) {
-        values.push(button.classList[2]);
-    } else {
-        values[values.length - 1] = button.classList[2];
-    }
-    clearInput();
-};
-
-// ----------------- BUTTON LOGIC -----------------
-
-let values = [];
+// creating lists of buttons
 for (let button of buttons) {
-    // create values array for calculations
-
-    // ----------------- NUMBER INPUT -----------------
+    // create event listeners for all
     if ('0123456789'.includes(button.classList[1])) {
-        button.addEventListener('click', (e) => {
-            const val = e.target.classList[1];
-            printNumber(val);
-        });
-    }
-    // ----------------- BACKSPACE -----------------
-    if (button.classList[1] === 'delete') {
-        button.addEventListener('click', delOneChar);
-    }
-
-    // ----------------- CLEAR INPUT -----------------
-    if (button.classList[1] === 'reset') {
-        button.addEventListener('click', () => {
-            clearInput();
-            values = [];
-        });
-    }
-    // ----------------- ADD DECIMAL -----------------
-    if (button.classList[1] === 'decimal-point') {
-        button.addEventListener('click', addDecimal);
-    }
-
-    // ----------------- ADD OPERATIONS -----------------
-    ['addition', 'subtraction', 'product', 'division'].forEach((operation) => {
-        if (button.classList[1] === operation) {
-            button.addEventListener('click', pushToOperationsArray);
-        }
-    });
-
-    if (button.classList[1] === 'equals') {
-        button.addEventListener('click', calculateValue);
+        numericalButtons.push(button);
+    } else if ('+-*/'.includes(button.classList[2])) {
+        operationButtons.push(button);
     }
 }
 
-// ----------------- KEYBOARD LOGIC  -----------------
-
-document.addEventListener('keydown', (e) => {
-    // ----------------- NUMBER INPUT -----------------
-    if ('0123456789'.includes(e.key)) {
-        printNumber(e.key);
+// ----------------- ES6 CALCULATOR CLASS -----------------
+class Calculator {
+    constructor() {
+        this.initialValue = 0;
+        this.secondValue = 0;
+        this.operation = '';
     }
 
-    // ----------------- BACKSPACE -----------------
-    if (e.key === 'Backspace') {
-        delOneChar();
+    insertNumber(value) {
+        if (this.operation === '') {
+            if (this.initialValue === 0) {
+                this.initialValue = value;
+            } else {
+                this.initialValue += value;
+            }
+            this.printToScreen('initial');
+
+            console.log('Initial', this.initialValue);
+        } else {
+            screenText.innerText = this.secondValue;
+            if (this.secondValue === 0) {
+                this.secondValue = value;
+            } else {
+                this.secondValue += value;
+            }
+            this.printToScreen('second');
+
+            console.log('Second', this.secondValue);
+        }
     }
 
-    // ----------------- ADD DECIMAL -----------------
-    if (e.key === '.') {
-        addDecimal();
+    insertOperation(value) {
+        if (this.operation !== '') {
+            this.calculate();
+        }
+        this.operation = value;
+
+        console.log(this.operation);
     }
 
-    // ----------------- ADD OPERATIONS -----------------
+    calculate() {
+        if (this.operation === '') {
+            return;
+        }
 
-    // ----------------- ADD ENTER KEY (=) -----------------
-    if (e.key === 'Enter') {
-        calculateValue(button);
+        const result = eval(`${this.initialValue} ${this.operation} ${this.secondValue}`);
+        console.log('calculation', result);
+        this.initialValue = result;
+        this.secondValue = 0;
+        this.printToScreen('initial');
+
+        console.log(this.initialValue, this.secondValue);
     }
+
+    resetValues() {
+        this.initialValue = 0;
+        this.secondValue = 0;
+        this.operation = '';
+        this.printToScreen('initial');
+    }
+
+    addDecimalPoint() {
+        if (this.operation === '') {
+            if (!this.initialValue.includes('.')) {
+                this.initialValue += '.';
+                this.printToScreen('initial');
+            }
+        } else {
+            if (!this.secondValue.includes('.')) {
+                this.secondValue += '.';
+                this.printToScreen('second');
+            }
+        }
+    }
+
+    delOneChar() {
+        if (this.operation === '') {
+            if (this.initialValue === 0) {
+                return;
+            } else if (this.initialValue.length === 1) {
+                this.initialValue = 0;
+            } else {
+                this.initialValue = this.initialValue.slice(0, this.initialValue.length - 1);
+            }
+            this.printToScreen('initial');
+        } else {
+            if (this.secondValue.length === 1) {
+                this.secondValue = 0;
+            } else {
+                this.secondValue = this.secondValue.slice(0, this.secondValue.length - 1);
+            }
+            this.printToScreen('second');
+        }
+        console.log(this.initialValue, this.secondValue);
+    }
+
+    printToScreen(which) {
+        if (which === 'initial') {
+            screenText.innerText = this.initialValue;
+        } else if (which === 'second') {
+            screenText.innerText = this.secondValue;
+        }
+    }
+}
+
+const calc = new Calculator();
+
+// ----------------- BUTTON EVENT LISTENERS -----------------
+
+// event listeners for numerical buttons
+numericalButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const value = button.classList[1];
+
+        calc.insertNumber(value);
+    });
+});
+
+// event listeners for operation buttons
+operationButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const operation = button.classList[2];
+
+        calc.insertOperation(operation);
+    });
+});
+
+// event listener for reset button
+resetButton.addEventListener('click', () => {
+    calc.resetValues();
+});
+
+// event listener for equals button
+equalsButton.addEventListener('click', () => {
+    calc.calculate();
+});
+
+// event listener for del button
+delButton.addEventListener('click', () => {
+    calc.delOneChar();
+});
+
+// event listener for decimal point button
+decimalButton.addEventListener('click', () => {
+    calc.addDecimalPoint();
 });
